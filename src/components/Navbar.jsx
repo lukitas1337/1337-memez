9ccf8712f6da74eae24308de6ca4f7fded581a6f
Lukitas <lvd.follert@gmail.com>
2024-10-15 15:19:31 +0200

    cleanup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,40 +7,8 @@ const Navbar = () => {
         <li className="text-warning">YO MAMAS FAV MEMES</li>
       </ul>
     </div>
-
-
-    <button className="btn btn-primary">Primary</button>
-    <button className="btn btn-secondary">Secondary</button>
-    <button className="btn btn-accent">Accent</button>
-    <button className="btn btn-neutral">Neutral</button>
-    <button className="btn btn-base-100">Base-100</button>
-    <button className="btn btn-info">Info</button>
-    <button className="btn btn-success">Success</button>
-    <button className="btn btn-warning">Warning</button>
-    <button className="btn btn-error">Error</button>
     </>
   )
 }
 
 export default Navbar
-
-
-
-
-// "primary": "#4ef379",
-          
-// "secondary": "#fc78c5",
-          
-// "accent": "#8be9fd",
-          
-// "neutral": "#f8f8f2",
-          
-// "base-100": "#282a36",
-          
-// "info": "#b84ced",
-          
-// "success": "#4ff97b",
-          
-// "warning": "#eef88a",
-          
-// "error": "#ff5555",
--- src/pages/MemeMachine.jsx
+++ src/pages/MemeMachine.jsx
@@ -1,15 +1,15 @@
 const MemeMachine = () => {
   return (
     <>
-    <section className="memeMachine text-center border-2 border-red-500">
-      <div className="controlRoom h-auto w-auto border-2 rounded-md flex justify-center align-middle">
-        <div className="imageSide border-2 border-red-500">
-          <input className="imgName w-full input input-bordered input-base-100" type="text" placeholder="I was told there would be" />
+    <section className="memeMachine text-center">
+      <div className="controlRoom rounded-md flex justify-center align-middle">
+        <div className="imageSide">
+          <input className="imgName w-full input input-bordered input-base-100 my-4" type="text" placeholder="I was told there would be" />
           <div className="imageContainer">
             <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
           </div>
         </div>
-        <div className="controlSide border-2 border-blue-500 p-16">
+        <div className="controlSide p-16">
           <div className="upperButtonContainer flex justify-around">
             <button className="uplBtn btn btn-accent">UPLOAD MA MEMEZ</button>
             <button className="rndmBtn btn btn-warning">GIMME RANDOM</button>

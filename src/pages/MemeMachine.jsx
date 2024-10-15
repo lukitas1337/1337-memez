ab4d329450152eba4de98e31f6f27d496c974cc5
Lukitas <lvd.follert@gmail.com>
2024-10-15 14:15:46 +0200

    mememachine basic layout

--- src/pages/MemeMachine.jsx
+++ src/pages/MemeMachine.jsx
@@ -1,25 +1,25 @@
 const MemeMachine = () => {
   return (
     <>
-    <section className="memeMachine">
-      <div className="controlRoom h-auto w-auto border-2 rounded-md">
-        <div className="imageSide">
-          <h3>I was told there would be</h3>
+    <section className="memeMachine text-center border-2 border-red-500">
+      <div className="controlRoom h-auto w-auto border-2 rounded-md flex justify-center align-middle">
+        <div className="imageSide border-2 border-red-500">
+          <input className="imgName w-full input input-bordered input-base-100" type="text" placeholder="I was told there would be" />
           <div className="imageContainer">
             <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
           </div>
         </div>
-        <div className="controlSide">
-          <div className="upperButtonContainer">
+        <div className="controlSide border-2 border-blue-500 p-16">
+          <div className="upperButtonContainer flex justify-around">
             <button className="uplBtn btn btn-accent">UPLOAD MA MEMEZ</button>
             <button className="rndmBtn btn btn-warning">GIMME RANDOM</button>
-            <input className="searchInput input input-bordered input-base-100" type="text" placeholder="SEARCH_MEMEZ" />
           </div>
+            <input className="searchInput w-full input input-bordered input-base-100 mt-6" type="text" placeholder="SEARCH_MEMEZ" />
           <div className="inputContainer">
-            <input className="topInput input input-bordered input-primary" type="text" placeholder="TOP_TEXT" />
-            <input className="btmInput input input-bordered input-secondary" type="text" placeholder="BOTTOM_TEXT" />
+            <input className="topInput w-full input input-bordered input-primary mt-6" type="text" placeholder="TOP_TEXT" />
+            <input className="btmInput w-full input input-bordered input-secondary my-6" type="text" placeholder="BOTTOM_TEXT" />
           </div>
-          <div className="lowerButtonContainer">
+          <div className="lowerButtonContainer flex justify-around">
             <button className="genBtn btn btn-success">I MAKE THIS</button>
             <button className="resBtn btn btn-error">GET OUTTA HERE</button>
           </div>

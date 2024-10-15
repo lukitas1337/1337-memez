56723c9e2ade1069cbfb501cd457b9f30b29941b
Lukitas <lvd.follert@gmail.com>
2024-10-15 15:03:10 +0200

    grid layout

--- src/pages/YoMamasFavMemes.jsx
+++ src/pages/YoMamasFavMemes.jsx
@@ -1,9 +1,93 @@
 const YoMamasFavMemes = () => {
   return (
     <>
-    <section className="flex justify-center">
-      <h1>IT'S YO MAMAS FAV MEMES OF ALL TIMES!</h1>
-      <div className="gallery">
+    <section className="text-center px-24">
+      <h1 className="text-7xl text-warning p-8">IT&apos;S YO MAMAS FAV MEMES OF ALL TIMES!</h1>
+      <div className="gallery grid grid-cols-4 gap-4">
+        <div className="memeContainer border-2 text-center">
+          <h3 className="memeName text-sm p-4">I was told there would be</h3>
+          <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
+          <div className="expMessage pt-4">
+          <p className="memeExp">THIS MEME IS DOOMED IN:</p>
+          <p className="memeExpTimer">00 DAYS 00 MINUTES 00 SECONDS</p>
+          </div>
+          <div className="memeButtons flex justify-between p-8">
+            <button className="dwnMeme btn btn-success">SAVE MEME</button>
+            <button className="delMeme btn btn-error">GO TO HELL</button>
+          </div>
+        </div>
+        <div className="memeContainer border-2 text-center">
+          <h3 className="memeName text-sm p-4">I was told there would be</h3>
+          <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
+          <div className="expMessage pt-4">
+          <p className="memeExp">THIS MEME IS DOOMED IN:</p>
+          <p className="memeExpTimer">00 DAYS 00 MINUTES 00 SECONDS</p>
+          </div>
+          <div className="memeButtons flex justify-between p-8">
+            <button className="dwnMeme btn btn-success">SAVE MEME</button>
+            <button className="delMeme btn btn-error">GO TO HELL</button>
+          </div>
+        </div>
+        <div className="memeContainer border-2 text-center">
+          <h3 className="memeName text-sm p-4">I was told there would be</h3>
+          <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
+          <div className="expMessage pt-4">
+          <p className="memeExp">THIS MEME IS DOOMED IN:</p>
+          <p className="memeExpTimer">00 DAYS 00 MINUTES 00 SECONDS</p>
+          </div>
+          <div className="memeButtons flex justify-between p-8">
+            <button className="dwnMeme btn btn-success">SAVE MEME</button>
+            <button className="delMeme btn btn-error">GO TO HELL</button>
+          </div>
+        </div>
+        <div className="memeContainer border-2 text-center">
+          <h3 className="memeName text-sm p-4">I was told there would be</h3>
+          <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
+          <div className="expMessage pt-4">
+          <p className="memeExp">THIS MEME IS DOOMED IN:</p>
+          <p className="memeExpTimer">00 DAYS 00 MINUTES 00 SECONDS</p>
+          </div>
+          <div className="memeButtons flex justify-between p-8">
+            <button className="dwnMeme btn btn-success">SAVE MEME</button>
+            <button className="delMeme btn btn-error">GO TO HELL</button>
+          </div>
+        </div>
+        <div className="memeContainer border-2 text-center">
+          <h3 className="memeName text-sm p-4">I was told there would be</h3>
+          <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
+          <div className="expMessage pt-4">
+          <p className="memeExp">THIS MEME IS DOOMED IN:</p>
+          <p className="memeExpTimer">00 DAYS 00 MINUTES 00 SECONDS</p>
+          </div>
+          <div className="memeButtons flex justify-between p-8">
+            <button className="dwnMeme btn btn-success">SAVE MEME</button>
+            <button className="delMeme btn btn-error">GO TO HELL</button>
+          </div>
+        </div>
+        <div className="memeContainer border-2 text-center">
+          <h3 className="memeName text-sm p-4">I was told there would be</h3>
+          <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
+          <div className="expMessage pt-4">
+          <p className="memeExp">THIS MEME IS DOOMED IN:</p>
+          <p className="memeExpTimer">00 DAYS 00 MINUTES 00 SECONDS</p>
+          </div>
+          <div className="memeButtons flex justify-between p-8">
+            <button className="dwnMeme btn btn-success">SAVE MEME</button>
+            <button className="delMeme btn btn-error">GO TO HELL</button>
+          </div>
+        </div>
+        <div className="memeContainer border-2 text-center">
+          <h3 className="memeName text-sm p-4">I was told there would be</h3>
+          <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />
+          <div className="expMessage pt-4">
+          <p className="memeExp">THIS MEME IS DOOMED IN:</p>
+          <p className="memeExpTimer">00 DAYS 00 MINUTES 00 SECONDS</p>
+          </div>
+          <div className="memeButtons flex justify-between p-8">
+            <button className="dwnMeme btn btn-success">SAVE MEME</button>
+            <button className="delMeme btn btn-error">GO TO HELL</button>
+          </div>
+        </div>
         <div className="memeContainer border-2 text-center">
           <h3 className="memeName text-sm p-4">I was told there would be</h3>
           <img src="https://a.pinatafarm.com/590x462/fb98437b64/i-was-told-there-would-be.jpg" alt="" />

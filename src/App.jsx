4deaf83c878e351478a030289e9cf53fc99a68c3
Lukitas <lvd.follert@gmail.com>
2024-10-15 17:37:28 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import Navbar from "./components/Navbar"
-import MemeMachine from "./pages/MemeMachine"
-import {MemeProvider} from "./context/MemeContext"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import MemeMachine from "./pages/MemeMachine";
+import YomamasFavMemes from "./pages/YomamasFavMemes"; // Import the new component
+import { MemeProvider } from "./context/MemeContext";
 
 const App = () => {
   return (
     <>
-    <MemeProvider>
-      <MemeMachine />
-    </MemeProvider>
-    </>
-  )
-}
+      <MemeProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            {/* Route for MemeMachine */}
+            <Route path="/" element={<MemeMachine />} />
 
-export default App
+            {/* Route for YomamasFavMemes */}
+            <Route path="/yomamasfavmemes" element={<YomamasFavMemes />} />
+          </Routes>
+        </Router>
+      </MemeProvider>
+    </>
+  );
+};
+
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <>
-    <div className="nav p-6">
-      <ul className="flex justify-center gap-8">
-        <li className="text-info">MEME MACHINE</li>
-        <li className="text-warning">YO MAMAS FAV MEMES</li>
-      </ul>
-    </div>
+      <div className="nav p-6">
+        <ul className="flex justify-center gap-8">
+          <li>
+            <Link to="/" className="text-info">MEME MACHINE</Link>
+          </li>
+          <li>
+            <Link to="/yomamasfavmemes" className="text-warning">YO MAMAS FAV MEMES</Link>
+          </li>
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

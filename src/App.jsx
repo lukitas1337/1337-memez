766649cee98a8928f26b7a32c9604f022b96793c
Lukitas <lvd.follert@gmail.com>
2024-10-15 13:01:24 +0200

    fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 const App = () => {
   return (
     <>
-    <button className="btn btn-primary">Moin</button>
-    <div className="text-3xl font-bold underline">App</div>
+    <div>App</div>
     </>
   )
 }
